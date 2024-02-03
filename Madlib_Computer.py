04e5988c6f2678c2fee2b9f1f5f7086b1132c5e6
Bharath Deva Dharshan <158748759+BharathDD-r@users.noreply.github.com>
2024-02-03 21:52:02 +0530

    fix : changed print text

--- Madlib_Computer.py
+++ Madlib_Computer.py
@@ -10,13 +10,13 @@ def computer_find(x):
             computer_guess = random.randint(lower_limit, higher_limit)
         else:
             computer_guess = higher_limit
-        remark = input(f" is {computer_guess} (h) (l) or (c)?: ")
+        remark = input(f"Is the Number {computer_guess} higer(h), lower(l) or correct(c)? : ")
         if remark == "h":
             higher_limit = computer_guess - 1
         elif remark == "l":
             lower_limit = computer_guess + 1
 
-    print(f" congo you guessed the number correctly")
+    print(f"Congragulations you guessed the number {computer_guess} correctly")
 
 
 computer_find(10)
